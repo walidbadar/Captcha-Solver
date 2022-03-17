09b5e70186c773327cedcd6aa43f43b624b095f0
walidbadar <81442784+walidbadar@users.noreply.github.com>
2022-03-18 02:15:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-import urllib.request, time
+import urllib.request, time, pyautogui
 
 inc=0
 
@@ -27,7 +27,7 @@ def recaptcha_login():
     # driver = webdriver.Chrome(resource_path('./driver/chromedriver.exe'),options=options)
     driver = webdriver.Chrome(options=options)
     driver.get('https://www.google.com/recaptcha/api2/demo')
-
+    time.sleep(5)
     WebDriverWait(driver, 20).until(ec.frame_to_be_available_and_switch_to_it(
         (By.CSS_SELECTOR, "iframe[src^='https://www.google.com/recaptcha/api2/anchor']")))
     WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.CSS_SELECTOR, "span#recaptcha-anchor"))).click()
@@ -40,14 +40,25 @@ def recaptcha_login():
     time.sleep(5)
     WebDriverWait(driver, 20).until(
         ec.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[7]/a"))).click()
-    time.sleep(2)
     for handle in driver.window_handles:
         inc = inc+1
         driver.switch_to.window(handle)
         if inc==1:
             url=driver.current_url
-            urllib.request.urlretrieve(url, "file.mp3")
+            urllib.request.urlretrieve(url, "d://file.mp3")
             inc=0
+
+    driver.get('https://speech-to-text-demo.ng.bluemix.net/')
+    WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.XPATH, "//*[@id=\"root\"]/div/div[6]/button[2]"))).click()
+    time.sleep(1)
+    pyautogui.click(397,598)
+    pyautogui.write('D:\\file.mp3')
+    pyautogui.press('enter')
+    xpath_text="//*[@id=\"root\"]/div/div[7]/div/div"
+    time.sleep(10)
+    WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.XPATH, xpath_text)))
+    stt=driver.find_element(By.XPATH, xpath_text).text
+    print(stt)
 
 # Login recaptcha Account
 recaptcha_login()
